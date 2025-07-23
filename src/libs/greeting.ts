6d1dc95ffb57d13a89c2aecf0037f1af92f920e0
Lars <larstech@proton.me>
2025-07-23 16:08:07 +0200

    feat: add greeting by time

---
+++ src/libs/greeting.ts
@@ -0,0 +1,24 @@
+import { inRange } from "@/utils/number"
+
+const MORNING_START = 6
+const MORNING_END = 12
+const AFTERNOON_START = 12
+const AFTERNOON_END = 18
+const EVENING_START = 18
+const EVENING_END = 0
+
+const getGreetingByTime = () => {
+  const currentHour = new Date().getHours()
+
+  if (inRange(currentHour, MORNING_START, MORNING_END)) {
+    return "Goedemorgen"
+  } else if (inRange(currentHour, AFTERNOON_START, AFTERNOON_END)) {
+    return "Goedemiddag"
+  } else if (inRange(currentHour, EVENING_START, EVENING_END)) {
+    return "Goedenavond"
+  } else {
+    return "Goedendag"
+  }
+}
+
+export { getGreetingByTime }
