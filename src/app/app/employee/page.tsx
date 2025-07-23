19637f41bde47f6962d319bf0613604a0ceaf452
Lars <larstech@proton.me>
2025-07-23 22:09:15 +0200

    refactor: run prettier

--- src/app/app/employee/page.tsx
+++ src/app/app/employee/page.tsx
@@ -1,4 +1,4 @@
-import EmployeeOverviewPage from "@/components/employee/overview-page";
+import EmployeeOverviewPage from "@/components/employee/overview-page"
 
 export default function Employee() {
   return <EmployeeOverviewPage />
