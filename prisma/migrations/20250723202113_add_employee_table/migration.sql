2ccdb413355f359289f9c50761ebb5e38046e82c
Lars <larstech@proton.me>
2025-07-24 12:19:56 +0200

    feat: add employee migration

---
+++ prisma/migrations/20250723202113_add_employee_table/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Employee" (
+    "email" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+
+    CONSTRAINT "Employee_pkey" PRIMARY KEY ("email")
+);
