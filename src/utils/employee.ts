45bc846fac61d02ca01b834b4bbf3bd755e918d7
Lars <larstech@proton.me>
2025-07-24 13:06:49 +0200

    refactor: run prettier

--- src/utils/employee.ts
+++ src/utils/employee.ts
@@ -1,15 +1,15 @@
-import { Employee } from "@/generated/prisma/client";
+import { Employee } from "@/generated/prisma/client"
 
 const sortEmployeesByName = (employees: Employee[]) => {
   return employees.sort((a, b) => {
-    const firstNameComparison = a.firstName.localeCompare(b.firstName);
+    const firstNameComparison = a.firstName.localeCompare(b.firstName)
 
     if (firstNameComparison !== 0) {
-      return firstNameComparison;
+      return firstNameComparison
     }
 
-    return a.lastName.localeCompare(b.lastName);
-  });
-};
+    return a.lastName.localeCompare(b.lastName)
+  })
+}
 
 export { sortEmployeesByName }
