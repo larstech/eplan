f812419ceb3b64eb06653358fd24fc1bf043e208
Lars <larstech@proton.me>
2025-07-31 14:13:45 +0200

    refactor: simplified app router

--- src/app/app/employee/page.tsx
+++ src/app/app/employee/page.tsx
@@ -1,5 +1,5 @@
 import EmployeeOverviewPage from "@/components/employee/overview-page"
 
-export default function Employee() {
+export default function Page() {
   return <EmployeeOverviewPage />
 }
