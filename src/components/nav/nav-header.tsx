f7d97b802b5fed387e5882083210d4c66bcb44da
Lars <larstech@proton.me>
2025-08-03 20:18:13 +0200

    feat: add breadcrumbs to the current url in the nav header

--- src/components/nav/nav-header.tsx
+++ src/components/nav/nav-header.tsx
@@ -1,25 +1,55 @@
+"use client"
+
 import {
   Breadcrumb,
   BreadcrumbItem,
+  BreadcrumbLink,
   BreadcrumbList,
   BreadcrumbPage,
+  BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb"
 import { Separator } from "@/components/ui/separator"
 import { SidebarTrigger } from "@/components/ui/sidebar"
+import { getPathnameData } from "@/utils/pathname"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import React from "react"
 
 export default function NavHeader() {
+  const pathname = usePathname()
+  const pathnameData = getPathnameData(pathname)
+
   return (
     <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
       <SidebarTrigger className="-ml-1" />
+
       <Separator
         orientation="vertical"
         className="mr-2 data-[orientation=vertical]:h-4"
       />
+
       <Breadcrumb>
         <BreadcrumbList>
-          <BreadcrumbItem>
-            <BreadcrumbPage>Startpagina</BreadcrumbPage>
-          </BreadcrumbItem>
+          {pathnameData.map((data, index) => {
+            if (data.current) {
+              return (
+                <BreadcrumbItem>
+                  <BreadcrumbPage>{data.section}</BreadcrumbPage>
+                </BreadcrumbItem>
+              )
+            }
+
+            return (
+              <React.Fragment key={index}>
+                <BreadcrumbItem>
+                  <BreadcrumbLink asChild>
+                    <Link href={data.url}>{data.section}</Link>
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+              </React.Fragment>
+            )
+          })}
         </BreadcrumbList>
       </Breadcrumb>
     </header>
