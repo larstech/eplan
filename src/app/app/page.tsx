5d9a1ba6a198eaa0d2c16e195507c67f5d4f07d0
Lars <larstech@proton.me>
2025-08-04 19:00:10 +0200

    refactor: rename app router exports to page

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,6 +1,6 @@
 import { HardHat, Wrench } from "lucide-react"
 
-export default function Home() {
+export default function Page() {
   return (
     <main className="container mx-auto flex p-2">
       <p className="pr-2">Er wordt momenteel aan deze applicatie gewerkt...</p>
--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,5 +1,5 @@
 import { LoginPage } from "@/components/auth/login-page"
 
-export default function Login() {
+export default function Page() {
   return <LoginPage />
 }
