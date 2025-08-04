0c54c41b914ad78cc9c72c1e46f0e687bb95dcef
Lars <larstech@proton.me>
2025-08-04 20:53:40 +0200

    refactor: clean up layouts

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -2,8 +2,8 @@ type LayoutParams = Readonly<{ children: React.ReactNode }>
 
 export default function Layout({ children }: LayoutParams) {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">{children}</div>
+    <div className="flex min-h-svh items-center justify-center p-6">
+      <main className="max-w-sm">{children}</main>
     </div>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,10 @@
 import type { Metadata } from "next"
-import { Geist, Geist_Mono } from "next/font/google"
 import "@/styles/globals.css"
 import { Analytics } from "@vercel/analytics/next"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Toaster } from "@/components/ui/sonner"
 
 type LayoutParams = Readonly<{ children: React.ReactNode }>
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-})
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-})
 
 export const metadata: Metadata = {
   title: "Elektronisch Planbord",
@@ -26,9 +15,7 @@ export const metadata: Metadata = {
 export default function Layout({ children }: LayoutParams) {
   return (
     <html lang="nl">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         {children}
 
         <Toaster />
