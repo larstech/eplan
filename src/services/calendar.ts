62fc245d97fd28c62855907d863df84283f9d337
Lars <larstech@proton.me>
2025-08-19 21:46:25 +0200

    feat: add calendar resource backend

--- src/services/calendar.ts
+++ src/services/calendar.ts
@@ -1,7 +1,37 @@
 "use server"
 
+import { getEmployeeById } from "./employee"
+import { getJobByOrderId } from "./job"
 import { prisma } from "@/libs/prisma"
-import { Calendar } from "@/types/calendar"
+import { Calendar, CalendarBulk } from "@/types/calendar"
+import { DateTime } from "luxon"
+import { toast } from "sonner"
+
+export const createCalendarFromBulk = async (calendarBulk: CalendarBulk) => {
+  calendarBulk.employees.forEach(async (employee) => {
+    const fetchedJob = await getJobByOrderId(Number(calendarBulk.orderId))
+    const fetchedEmployee = await getEmployeeById(employee.employeeId)
+
+    if (!fetchedJob) {
+      toast(`Ordernummer '${calendarBulk.orderId}' niet gevonden.`)
+      return
+    }
+    if (!fetchedEmployee) {
+      toast("Medewerker(s) niet gevonden.")
+      return
+    }
+
+    employee.dates.forEach(async (date) => {
+      await createCalendar({
+        job: fetchedJob,
+        employee: fetchedEmployee,
+        date: DateTime.fromISO(date).toJSDate(),
+        startTime: DateTime.fromISO(employee.startTime).toJSDate(),
+        endTime: DateTime.fromISO(employee.endTime).toJSDate(),
+      })
+    })
+  })
+}
 
 export const createCalendar = async (calendar: Calendar) => {
   const createdCalendar = await prisma.calendar.create({
