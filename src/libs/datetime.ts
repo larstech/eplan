e72b6bf6f0dd522bd1e3709f1e035edf0deed6e5
Lars <larstech@proton.me>
2025-08-25 15:35:20 +0200

    docs: add documentation to CET/CEST date support

--- src/libs/datetime.ts
+++ src/libs/datetime.ts
@@ -14,6 +14,8 @@ const lastSundayInMonth = (month: number, day: number): DateTime => {
 // The code below relies on hard‑coded DST rules that can break in any year.
 // It assumes the EU will keep daylight saving. If they drop it, the logic will
 // silently return wrong times.
+//
+// Read more: https://en.wikipedia.org/wiki/Summer_time_in_Europe
 export const date = (date: Date): DateTime => {
   const dateTime = DateTime.fromJSDate(date, { zone: "UTC" }).setZone(
     "Europe/Amsterdam",
