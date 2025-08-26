96475c81579aa8f3e588ac48fb1cc70d1352c109
Lars <larstech@proton.me>
2025-08-26 12:14:01 +0200

    refactor: update 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,32 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import Image from "next/image"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
 export default function NotFound() {
-  return <span>Kan de resource niet vinden. Probeer het opnieuw.</span>
+  const pathname = usePathname()
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="max-w-md">
+        <Image
+          src="/yourtech-logo-full.png"
+          alt="Yourtech logo"
+          width="300"
+          height="1"
+        />
+        <h2 className="font-semibold">404. Pagina niet gevonden</h2>
+
+        <p>
+          De verzochte URL <em>{pathname}</em> was niet gevonden.
+        </p>
+
+        <Link href="/">
+          <Button className="mt-4">Ga terug naar de startpagina</Button>
+        </Link>
+      </div>
+    </div>
+  )
 }
