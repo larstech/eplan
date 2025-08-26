6a8a2b8a62e9948dc0c6d7603d48d7f38e50e775
Lars <larstech@proton.me>
2025-08-26 10:19:21 +0200

    refactor: add @default(now) to @updatedAt

---
+++ prisma/migrations/20250826075956_update_updated_at_column/migration.sql
@@ -0,0 +1,17 @@
+-- AlterTable
+ALTER TABLE "public"."Address" ALTER COLUMN "updatedAt" SET DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "public"."Calendar" ALTER COLUMN "updatedAt" SET DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "public"."Customer" ALTER COLUMN "updatedAt" SET DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "public"."CustomerContact" ALTER COLUMN "updatedAt" SET DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "public"."Employee" ALTER COLUMN "updatedAt" SET DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "public"."Job" ALTER COLUMN "updatedAt" SET DEFAULT CURRENT_TIMESTAMP;
