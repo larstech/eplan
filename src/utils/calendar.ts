85d5781b0d743791e02e636f43ef73d4d374295b
Lars <larstech@proton.me>
2025-08-31 22:44:18 +0200

    refactor: extract calendar export function

---
+++ src/utils/calendar.ts
@@ -0,0 +1,21 @@
+import * as htmlToImage from "html-to-image"
+import { DateTime } from "luxon"
+import { RefObject } from "react"
+
+export const exportCalendarToJpeg = (
+  date: DateTime,
+  tableRef: RefObject<HTMLTableElement | null>,
+) => {
+  const currentYear = date.year
+  const currentWeek = date.weekNumber
+  const exportFileName = `planbord_${currentYear}W${currentWeek}`
+
+  htmlToImage
+    .toPng(tableRef.current!, { quality: 1, skipFonts: true })
+    .then(function (dataUrl) {
+      const link = document.createElement("a")
+      link.download = `${exportFileName}.jpeg`
+      link.href = dataUrl
+      link.click()
+    })
+}
