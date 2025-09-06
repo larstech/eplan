0728bc83b286c87faad4d15da044caffc05243e4
Lars <larstech@proton.me>
2025-09-06 17:23:21 +0200

    refactor: change div to fragment in auth footer

--- src/features/auth/components/form/footer.tsx
+++ src/features/auth/components/form/footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 export default function FormFooter() {
   return (
-    <div>
+    <>
       <p className="text-sm text-muted-foreground">
         Deze omgeving is uitsluitend bedoeld voor medewerkers van Yourtech. Ben
         je geen medewerker, maar zoek je wel informatie over Yourtech?
@@ -11,6 +11,6 @@ export default function FormFooter() {
       <Link href="https://yourtech.nl/nl">
         <Button variant="link">Klik dan hier</Button>
       </Link>
-    </div>
+    </>
   )
 }
