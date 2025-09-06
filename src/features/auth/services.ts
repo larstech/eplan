9c378eaae54ae0228d5913f2924148b772a5ba25
Lars <larstech@proton.me>
2025-09-06 19:34:20 +0200

    refactor(auth): change lambdas to concrete functions

--- src/features/auth/services.ts
+++ src/features/auth/services.ts
@@ -4,17 +4,20 @@ import { createClient } from "@/lib/supabase/server"
 import { revalidatePath } from "next/cache"
 import { redirect } from "next/navigation"
 
-export const signIn = async (email: string, password: string) => {
+export async function signIn(email: string, password: string) {
   const supabase = await createClient()
-  const result = await supabase.auth.signInWithPassword({
+
+  const signInResult = await supabase.auth.signInWithPassword({
     email: email,
     password: password,
   })
-  return result
+
+  return signInResult
 }
 
-export const signOut = async () => {
+export async function signOut() {
   const supabase = await createClient()
+
   await supabase.auth.signOut({ scope: "local" })
 
   revalidatePath("/")
