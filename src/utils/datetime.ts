bb5133ada8265839f1ef7b9830435b18f6c8aa0e
Lars <larstech@proton.me>
2025-09-15 00:36:41 +0200

    refactor(style): convert most lambda's to concrete functions

--- src/utils/datetime.ts
+++ src/utils/datetime.ts
@@ -1,7 +1,9 @@
 import { DateTime } from "luxon"
 
-export const previousWeek = (dateTime: DateTime): DateTime =>
-  dateTime.minus({ week: 1 })
+export function previousWeek(dateTime: DateTime): DateTime {
+  return dateTime.minus({ week: 1 })
+}
 
-export const nextWeek = (dateTime: DateTime): DateTime =>
-  dateTime.plus({ week: 1 })
+export function nextWeek(dateTime: DateTime): DateTime {
+  return dateTime.plus({ week: 1 })
+}
