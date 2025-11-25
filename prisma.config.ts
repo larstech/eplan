19c2255259dd7e6d1abf5b10a240ef1e1ab1b934
larstech <larstech@proton.me>
2025-11-25 21:17:53 +0100

    build(prisma): update forcefully to 7.0.1

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,6 +2,9 @@ import "dotenv/config"
 import type { PrismaConfig } from "prisma"
 
 export default {
+  datasource: {
+    url: process.env.POSTGRES_PRISMA_URL!,
+  },
   schema: "prisma/schema.prisma",
   migrations: {
     seed: "tsx prisma/seed.ts",
