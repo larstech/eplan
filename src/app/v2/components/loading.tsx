282683b1f71392a5a94488458b98a42d2abb39e1
larstech <larstech@proton.me>
2026-01-09 21:00:56 +0100

    refactor(loading): replace absolute positioning with flex

--- src/app/v2/components/loading.tsx
+++ src/app/v2/components/loading.tsx
@@ -1,6 +1,6 @@
 export default function LoadingState() {
   return (
-    <div className="position-absolute top-50 start-50 translate-middle">
+    <div className="d-flex justify-content-center p-2">
       <div className="spinner-border" role="status">
         <span className="visually-hidden">Laden...</span>
       </div>
