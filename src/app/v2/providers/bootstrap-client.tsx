3e6fd9dd0c7007ac90dd14aa877b7a905e40f2bb
larstech <larstech@proton.me>
2026-01-10 13:42:11 +0100

    fix: add reason after @ts-expect-error

--- src/app/v2/providers/bootstrap-client.tsx
+++ src/app/v2/providers/bootstrap-client.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react"
 
 export default function BootstrapClient() {
   useEffect(() => {
-    // @ts-expect-error
+    // @ts-expect-error Bootstrap JS has no TypeScript types
     import("bootstrap/dist/js/bootstrap.bundle.min.js")
   }, [])
 
