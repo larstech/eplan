e39d4f603edc6ceafffac59706ab4bcfb2ca3481
larstech <larstech@proton.me>
2026-01-20 12:51:19 +0100

    feat: update layout structure with react-bootstrap

--- src/app/v2/layout.tsx
+++ src/app/v2/layout.tsx
@@ -1,13 +1,15 @@
+import BootstrapClient from "@/app/v2/providers/bootstrap-client"
 import "@/app/v2/styles/globals.css"
 import { ReactNode } from "react"
-import { Container } from "react-bootstrap"
 
 type LayoutParams = Readonly<{ children: ReactNode }>
 
 export default function Layout({ children }: LayoutParams) {
   return (
-    <Container fluid>
+    <>
+      <BootstrapClient />
+
       {children}
-    </Container>
+    </>
   )
 }
